5d8f5f0a1b9fa64439390831dd834d100dce5c6b
Marius Orcsik <marius@habarnam.ro>
2016-11-26 10:52:25 +0100

    Removed a useless import

--- source/app.d
+++ source/app.d
@@ -1,4 +1,3 @@
-import std.socket;
 import std.parallelism;
 import dhttp.server;
 
@@ -6,7 +5,6 @@ int main()
 {
     Server s = new Server(8080, 3);
 
-    //HTTPListen(s);
     auto task = task!HTTPListen(s);
     task.executeInNewThread();
 
